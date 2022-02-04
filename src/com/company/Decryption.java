b4f1026d1b8a41a611bfc65ad525e7f2b2051a55
tdettling <tdettling@tds.net>
2022-02-04 00:01:51 -0500

    COMPLIEEDDD

--- src/com/company/Decryption.java
+++ src/com/company/Decryption.java
@@ -15,42 +15,76 @@ public class Decryption {
         encryptedPhrase = new ArrayList<>();
         String tempPhrase = "pwv hbog pn en b mbdg xjzig rq tazl crfnd bok rszkzynk hruuna";
         for (int i = 0; i < tempPhrase.length(); i++) {
-            encryptedPhrase.add(tempPhrase.charAt(i));
+            this.encryptedPhrase.add(tempPhrase.charAt(i));
         }
     }
 
     public Decryption(int multiplier, int addedNum, String phrase) {
-        this.decryptionMultiplier = multiplier;
-        this.decryptionAddedNumber = addedNum;
+        if(isValidValue(multiplier)) {
+            this.decryptionMultiplier = multiplier;
+        }else{
+            this.decryptionMultiplier = 5;
+        }
+
+        if(isValidValue(addedNum)) {
+            this.decryptionAddedNumber = addedNum;
+        }else{
+            this.decryptionAddedNumber = 3;
+        }
+
+        if(isValidPhrase(phrase)){
+            encryptedPhrase = parsePhrase(phrase);
+        }else{
+            String tempPhrase = "pwv hbog pn en b mbdg xjzig rq tazl crfnd bok rszkzynk hruuna";
+            this.encryptedPhrase = parsePhrase(tempPhrase);
+        }
     }
 
-    public boolean isValidPhrase(String phrase) {
+    public boolean isValidPhrase(String phrase){
         boolean isPhrase = true;
-
+        ArrayList<Character> containsNumbers = new ArrayList<Character>();
+        for (int i = 0; i < 11 ; i++ ){
+            char iCount = (char)i;
+            for (int k = 0; k < phrase.length() ; k++ ){
+                if(phrase.charAt(k) == iCount){
+                    isPhrase = false;
+                    containsNumbers.add(phrase.charAt(k));
+                }
+            }
+        }
+        System.out.println("Your phrase to decrypt was not a valid input. It contained the following numbers: ");
+        for (Character c : containsNumbers) {
+            System.out.print(" " + c);
+        }
         return isPhrase;
     }
 
-
-    /*public boolean isValidPangram(String phrase) {
-        boolean isPangram = true;
-        ArrayList<Character> doesNotContainLetters = new ArrayList<Character>();
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-            // Check if the string does not
-            // contain all the letters
-            if (!phrase.contains(String.valueOf(ch))) {
-                isPangram = false;
-                doesNotContainLetters.add(ch);
-            }
+    public boolean isValidValue(int value){
+        boolean isValidValue = true;
+        if(value<0||value>25){
+            System.out.println(" Your input of " + value + " was not between 0 and 25. Your input has been set to the default value.");
+            isValidValue = false;
         }
-
-
-        System.out.println("Your phrase was not a valid input. it did not have the following letters: ");
-        //loop through list to print off missing characters
-        for (Character c : doesNotContainLetters) {
-            System.out.print(" " + c);
-        }
-        return isPangram;
+        return isValidValue;
     }
-*/
+
+    public ArrayList<Character> parsePhrase(String phrase) {
+        //maybe add logic to allow users to use punctuation, and then remove it here???
+        ArrayList<Character> parsedPhrase = new ArrayList<Character>();
+        phrase = phrase.toLowerCase();
+
+        //1. Take phrase and remove all non-letters out of it
+        //fixme
+        //2. Parse
+
+        // Take each character of the given string and save it as an entry inside a
+        // temporary ArrayList, which will contain our parsed pangram phrase for the constructor.
+        for (int i = 0; i < phrase.length(); i++) {
+            // Adding each character of phrase into ArrayList
+            parsedPhrase.add(phrase.charAt(i));
+        }
+        return parsedPhrase;
+    }
+
 
 }
--- src/com/company/Encryption.java
+++ src/com/company/Encryption.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.ArrayList;
 
 public class Encryption {
-    int encryptionKey, encryptionRemainder;
+    int encryptionMultiplier, encryptionRemainder;
     final String alphabet = "abcdefghijklmnopqrstuvwxyz";
     ArrayList<Character> phraseNotEncrypted;
 
@@ -14,20 +14,28 @@ public class Encryption {
     Parses string into ArrayList
      */
     public Encryption() {
-        this.encryptionKey = 3;
+        this.encryptionMultiplier = 3;
         this.encryptionRemainder = 2;
         phraseNotEncrypted = new ArrayList<Character>();
         this.phraseNotEncrypted = parsePhrase("why cant we be a vast quilt of grim jokes and oxidized copper");
+
     }
 
     public Encryption(int key, int remainder,String phrase){
-        this.encryptionKey = 3;
-        this.encryptionRemainder = remainder;
+        int gcf = GCF(key, remainder);
+        if(gcf != 1){
+            System.out.println("Your Cipher inputs were invalid, because the GCF is "+gcf);
+            System.out.println("Your multiplier will be 5 and remainder will be 3.");
+            this.encryptionMultiplier = 3;
+            this.encryptionRemainder = 2;
+        }else {
+            this.encryptionMultiplier = key;
+            this.encryptionRemainder = remainder;
+        }
         phraseNotEncrypted = new ArrayList<Character>();
         //checking for a valid phrase, NOT VALID if:
             // there are numbers
             // there are symbols and things that are not letters
-
         this.phraseNotEncrypted = parsePhrase(phrase);
     }
     public boolean isValidPhrase(String phrase){
@@ -42,7 +50,7 @@ public class Encryption {
                 }
             }
         }
-        System.out.println("Your phrase was not a valid input. It contained the following numbers: ");
+        System.out.println("Your phrase to encrypt was not a valid input. It contained the following numbers: ");
         for (Character c : containsNumbers) {
             System.out.print(" " + c);
         }
@@ -65,5 +73,43 @@ public class Encryption {
                 parsedPhrase.add(phrase.charAt(i));
             }
             return parsedPhrase;
+    }
+
+    public int GCF(int a, int b){
+        //if gcf(26,encryptionMultiplier) > 1, then NOT valid.
+        if(a%b == 0){
+            return b;
+        }
+        else{
+            int q = (int)(a/b);
+            int r = a - (b*q);
+            return GCF(b, r);
+        }
+    }
+    public ArrayList<Character> encryptPhrase(ArrayList<Character> notEncrypted){
+        ArrayList<Character> encrypted = new ArrayList<Character>();
+
+        for(int i = 0; i< notEncrypted.size(); i++){
+            int x = (encryptionMultiplier*i+encryptionRemainder)%26;
+            encrypted.add(alphabet.charAt(x));
+        }
+        return encrypted;
+    }
+
+    public void printEncryptData(){
+        System.out.println("Multiplier: "+ encryptionMultiplier);
+        System.out.println("Remainder: "+encryptionRemainder);
+        System.out.println("Given phrase: ");
+        for (Character character : phraseNotEncrypted) {
+            System.out.print(" " + character);
+        }
+    }
+
+    public void printEncryption(){
+        ArrayList<Character> encryptedPhrase = new ArrayList<Character>();
+        encryptedPhrase = encryptPhrase(phraseNotEncrypted);
+        for( Character c: encryptedPhrase){
+            System.out.print(" "+ c);
+        }
     }
 }
